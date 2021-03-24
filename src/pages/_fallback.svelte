e64d05868dbefe259bf2117884feefd7332458f2
GHOST <ghostdevbusiness@gmail.com>
2021-03-24 20:53:19 +0000

    feat: 404 page

--- src/pages/_fallback.svelte
+++ src/pages/_fallback.svelte
@@ -1,2 +1,18 @@
-<!-- routify:options reset -->
-404
+<main>
+    <h1>404</h1>
+    <p>
+        Unfortunately you are looking for something doesn't exist :/
+        <a href="/">Want to go back?</a>
+    </p>
+</main>
+
+<style lang="scss">
+    main {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        text-align: center;
+    }
+</style>
