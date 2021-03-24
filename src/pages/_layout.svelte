18442ddfb56498ef801d48f6569c80104b2f18e4
GHOST <ghostdevbusiness@gmail.com>
2021-03-24 20:53:09 +0000

    feat: loading animation

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,6 +1,37 @@
 <script>
     import Particles from 'svelte-particles';
     import { config } from '@/particles.config.js';
+    import { fly, fade } from 'svelte/transition';
+    import { animationShown } from '@/stores.js';
+
+    const time = 1500;
+    const loading = new Promise((resolve) =>
+        $animationShown
+            ? resolve()
+            : setTimeout(() => {
+                  $animationShown = true;
+                  resolve();
+              }, time),
+    );
 </script>
 
 <Particles id="particles" options={config} />
+
+{#await loading}
+    <div class="loader" in:fly={{ y: -200, duration: time - 500 }} out:fade>
+        <h1>GHOST</h1>
+    </div>
+{:then}
+    <slot />
+{/await}
+
+<style lang="scss">
+    .loader {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        z-index: 100;
+    }
+</style>
