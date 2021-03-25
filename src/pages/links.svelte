427fd5c3d9bbb210acff93eaddf914901d3878d1
GHOST <ghostdevbusiness@gmail.com>
2021-03-25 00:30:53 +0000

    feat: links page

---
+++ src/pages/links.svelte
@@ -0,0 +1,81 @@
+<main>
+    <h3>Links</h3>
+    <p>All relevant and interesting links</p>
+
+    <div class="row contact">
+        <card>
+            <h6>Contact</h6>
+            <a href="/contact">Contact Page</a>
+        </card>
+        <card>
+            <h6>Github</h6>
+            <a href="https://github.com/ghostdevv">Github</a>
+        </card>
+    </div>
+</main>
+
+<style lang="scss">
+    @import 'style/helpers/media';
+
+    main {
+        padding: 32px 8px;
+
+        display: flex;
+        flex-flow: column nowrap;
+        align-items: center;
+
+        .row {
+            width: 100%;
+            padding: 8px 0px;
+        }
+    }
+
+    a,
+    p {
+        font-size: 1.7rem;
+    }
+
+    .contact {
+        display: flex;
+        justify-content: center;
+        flex-flow: row wrap;
+        align-items: center;
+        flex-basis: 25%;
+
+        card {
+            box-shadow: 1px 1px 5px 0px rgba(0, 0, 0, 0.1);
+            background-color: #2b2d30;
+            border: 2px solid #2b2d30;
+            padding: 32px;
+            margin: 8px;
+            border-radius: 16px;
+            min-width: 500px;
+            max-width: 750px;
+
+            cursor: pointer;
+
+            transition: 0.2s ease-in-out;
+
+            @include media('<1000px') {
+                min-width: 300px;
+            }
+
+            @include media('<640px') {
+                max-width: none;
+                width: 100%;
+            }
+
+            > * {
+                margin: 2px 0px;
+            }
+
+            text-align: center;
+
+            display: flex;
+            flex-flow: column nowrap;
+            align-items: center;
+            justify-content: center;
+            align-self: stretch;
+        }
+    }
+</style>
