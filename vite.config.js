afd0eb28d0d5253002806dbc8ba52094b883a822
GHOST <ghostdevbusiness@gmail.com>
2021-03-29 16:41:01 +0100

    feat: move website to vite

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import svelte from '@sveltejs/vite-plugin-svelte';
+import autoPreprocess from 'svelte-preprocess';
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default ({ mode }) =>
+    defineConfig({
+        server: {
+            port: 5001,
+        },
+        build: {
+            cssCodeSplit: false,
+            target: ['es2015'],
+        },
+        resolve: {
+            alias: [
+                {
+                    find: '@',
+                    replacement: path.resolve(__dirname, './src'),
+                },
+            ],
+        },
+        plugins: [
+            svelte({
+                preprocess: [
+                    autoPreprocess({
+                        postcss: require('./postcss.config.js'),
+                        defaults: { style: 'postcss' },
+                    }),
+                ],
+                emitCss: true,
+                hot: !mode == 'production',
+            }),
+        ],
+    });
