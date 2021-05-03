1e64010d25fc38329aba85f9637350b41a657d46
GHOST <ghostdevbusiness@gmail.com>
2021-05-03 02:10:16 +0100

    feat: update links

--- src/pages/links.svelte
+++ src/pages/links.svelte
@@ -3,15 +3,23 @@
     <p>All relevant and interesting links</p>
 
     <div class="row fl">
-        <a href="/contact"
-            ><card>
+        <a href="/contact">
+            <card>
                 <h6>Contact</h6>
-            </card></a>
+            </card>
+        </a>
 
-        <a href="https://github.com/ghostdevv"
-            ><card>
+        <a href="https://github.com/ghostdevv">
+            <card>
                 <h6>Github</h6>
-            </card></a>
+            </card>
+        </a>
+
+        <a href="/projects">
+            <card>
+                <h6>Projects</h6>
+            </card>
+        </a>
     </div>
 </main>
 
