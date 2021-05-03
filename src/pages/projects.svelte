50344a7473a800d511878a0eb1c735bf4fb111d3
GHOST <ghostdevbusiness@gmail.com>
2021-05-03 02:53:29 +0100

    feat: update projects

--- src/pages/projects.svelte
+++ src/pages/projects.svelte
@@ -1,20 +1,58 @@
 <script>
     const projects = [
         {
-            name: 'Rejxcted',
-            link: 'https://rejxcted.dev',
+            name: 'Websites',
+            items: [
+                {
+                    name: 'Rejxcted',
+                    link: 'https://rejxcted.dev',
+                },
+                {
+                    name: 'Arrow Alerts',
+                    link: 'https://arrowalerts.co',
+                },
+                {
+                    name: 'Mesh Monitors',
+                    link: 'https://meshmonitors.io',
+                },
+                {
+                    name: 'Mesh QT',
+                    link: 'https://meshmonitors.io/quicktasks',
+                },
+            ],
         },
         {
-            name: 'Arrow Alerts',
-            link: 'https://arrowalerts.co',
-        },
-        {
-            name: 'Mesh Monitors',
-            link: 'https://meshmonitors.io',
-        },
-        {
-            name: 'Mesh QT',
-            link: 'https://meshmonitors.io/quicktasks',
+            name: 'Open Source',
+            items: [
+                {
+                    name: 'Routify',
+                    link: 'https://routify.dev/',
+                },
+                {
+                    name: 'DashArgs',
+                    link: 'https://github.com/ghoststools/dashargs',
+                },
+                {
+                    name: 'SourceBin',
+                    link: 'https://github.com/ghoststools/sourcebin',
+                },
+                {
+                    name: 'djs-ticketsystem',
+                    link: 'https://github.com/ghoststools/djs-ticketsystem',
+                },
+                {
+                    name: 'Github 1s Button',
+                    link: 'https://github.com/ghostdevv/github1s-button',
+                },
+                {
+                    name: 'rollup-obfuscator',
+                    link: 'https://github.com/ghoststools/rollup-obfuscator',
+                },
+                {
+                    name: 'linode-object-upload',
+                    link: 'https://github.com/ghostdevv/linode-object-upload',
+                },
+            ],
         },
     ];
 </script>
@@ -23,17 +61,38 @@
     <h3>Projects</h3>
     <p>Here are a collection of projects that I have made</p>
 
-    <div class="row fl">
-        {#each projects as { name, link }}
-            <a href={link} target="_blank">
-                <card>
-                    <h6>{name}</h6>
-                </card>
-            </a>
-        {/each}
-    </div>
+    <div class="spacer" />
+
+    {#each projects as { name, items }}
+        <div class="project-row">
+            <h6>{name}</h6>
+            <div class="row fl">
+                {#each items as { name, link }}
+                    <a href={link} target="_blank">
+                        <card>
+                            <h6>{name}</h6>
+                        </card>
+                    </a>
+                {/each}
+            </div>
+        </div>
+    {/each}
 </main>
 
 <style lang="scss">
     @import 'style/ui/page';
+
+    .spacer {
+        padding: 8px;
+    }
+
+    .project-row {
+        display: flex;
+        flex-flow: column nowrap;
+        align-items: center;
+        text-align: center;
+
+        padding: 12px 0px;
+        gap: 8px;
+    }
 </style>
