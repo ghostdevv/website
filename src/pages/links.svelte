9f06e7fb85fd79b929e73630ae69bb1bad86b017
GHOST <ghostdevbusiness@gmail.com>
2021-05-04 20:25:18 +0100

    fix: external=false

--- src/pages/links.svelte
+++ src/pages/links.svelte
@@ -9,19 +9,19 @@
     <p>All relevant and interesting links</p>
 
     <Row>
-        <LinkCard url="/contact">
+        <LinkCard url="/contact" external={false}>
             <h6>Contact</h6>
         </LinkCard>
 
-        <LinkCard url="/donate">
+        <LinkCard url="/donate" external={false}>
             <h6>Donate</h6>
         </LinkCard>
 
-        <LinkCard url="https://github.com/ghostdevv">
+        <LinkCard url="https://github.com/ghostdevv" external={false}>
             <h6>Github</h6>
         </LinkCard>
 
-        <LinkCard url="/projects">
+        <LinkCard url="/projects" external={false}>
             <h6>Projects</h6>
         </LinkCard>
     </Row>
