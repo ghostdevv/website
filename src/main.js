ed48c023c76af2d23c6a65929358963a1c4a997e
GHOST <ghostdevbusiness@gmail.com>
2021-07-05 00:23:53 +0100

    refactor: move global to main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import HMR from '@roxi/routify/hmr'
+import '../style/global.scss';
+import HMR from '@roxi/routify/hmr';
 import App from './App.svelte';
 
-const app = HMR(App, { target: document.body }, 'oxova-app')
+const app = HMR(App, { target: document.body }, 'oxova-app');
 
 export default app;
