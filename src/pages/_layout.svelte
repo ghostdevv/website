11e92569fd448f799b633edcd0f26bc490f6e014
GHOST <ghostdevbusiness@gmail.com>
2021-07-05 01:54:22 +0100

    feat: layout

---
+++ src/pages/_layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Nav from '@/components/Nav.svelte';
+</script>
+
+<div class="wrapper">
+    <Nav />
+</div>
+
+<style>
+    .wrapper {
+        max-width: 1400px;
+        margin: 0px auto;
+    }
+</style>
