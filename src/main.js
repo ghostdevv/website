8174d881ddb87e1741e71e22139d0d3ae86c1c4d
GHOST <ghostdevbusiness@gmail.com>
2021-07-05 03:52:49 +0100

    chore: update site name

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import '../style/global.scss';
 import HMR from '@roxi/routify/hmr';
 import App from './App.svelte';
 
-const app = HMR(App, { target: document.body }, 'oxova-app');
+const app = HMR(App, { target: document.body }, 'ghosts-site');
 
 export default app;
