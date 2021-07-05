775c983ffbb95805f70c890959bcaf4f3e95a4f2
GHOST <ghostdevbusiness@gmail.com>
2021-07-05 02:53:27 +0100

    chore: placeholder

---
+++ src/pages/index.svelte
@@ -0,0 +1,11 @@
+<div>
+    <h1>Welcome</h1>
+    <p>Will put some content here</p>
+</div>
+
+<style lang="scss">
+    div {
+        text-align: center;
+        padding: 24px 0px;
+    }
+</style>
