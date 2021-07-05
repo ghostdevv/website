4100c1e9f323fe0962f8ac306c648c57e0e396f6
GHOST <ghostdevbusiness@gmail.com>
2021-07-05 03:46:47 +0100

    feat: projects

---
+++ src/pages/projects.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { os } from '@/projects.config.js';
+
+    const starred = os.filter((x) => x.star);
+    const rest = os.filter((x) => !x.star);
+</script>
+
+<div class="column center g32">
+    <h3>⭐ Starred Projects</h3>
+
+    <div class="row wrap center g16">
+        {#each starred as project}
+            <a href={project.url} target="_blank" class="card blue">
+                <h5>{project.name}</h5>
+            </a>
+        {/each}
+    </div>
+</div>
+
+<div class="column center g32">
+    <h3>OS Projects</h3>
+
+    <div class="row wrap center g16">
+        {#each rest as project}
+            <a href={project.url} target="_blank" class="card">
+                <h5>{project.name}</h5>
+            </a>
+        {/each}
+    </div>
+</div>
---
+++ src/projects.config.js
@@ -0,0 +1,53 @@
+export const os = [
+    {
+        name: 'Routify',
+        url: 'https://routify.dev/',
+        star: true,
+    },
+    {
+        name: 'Svelte Hamburgers',
+        url: 'https://github.com/ghostdevv/svelte-hamburgers',
+        star: true,
+    },
+    {
+        name: 'SourceBin',
+        url: 'https://github.com/ghoststools/sourcebin',
+    },
+    {
+        name: 'JellyCommands',
+        url: 'https://github.com/ghostdevv/jellycommands',
+        star: true,
+    },
+    {
+        name: 'DashArgs',
+        url: 'https://github.com/ghoststools/dashargs',
+    },
+    {
+        name: 'MyKV',
+        url: 'https://github.com/ghostdevv/mykv',
+    },
+    {
+        name: 'GHOSTs Tools',
+        url: 'https://github.com/ghostdevv/ghoststools',
+    },
+    {
+        name: 'djs-ticketsystem',
+        url: 'https://github.com/ghoststools/djs-ticketsystem',
+    },
+    {
+        name: 'Github 1s Button',
+        url: 'https://github.com/ghostdevv/github1s-button',
+    },
+    {
+        name: 'rollup-obfuscator',
+        url: 'https://github.com/ghoststools/rollup-obfuscator',
+    },
+    {
+        name: 'linode-object-upload',
+        url: 'https://github.com/ghostdevv/linode-object-upload',
+    },
+    {
+        name: 'linode-bucket-delete',
+        url: 'https://github.com/ghostdevv/linode-bucket-delete',
+    },
+];
