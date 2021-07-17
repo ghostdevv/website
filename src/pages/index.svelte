35524c51b32fe9bbbbe7f5cf5e37a9b14389810e
GHOST <ghostdevbusiness@gmail.com>
2021-07-17 01:40:22 +0100

    feat: home page updates

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,9 +1,33 @@
+<script>
+    import { fly } from 'svelte/transition';
+    import { links } from '@/home.config';
+</script>
+
 <div>
-    <h1>Welcome</h1>
-    <p>Will put some content here</p>
+    <div class="letter-row">
+        {#each 'GHOST' as c, i}
+            <h1 in:fly={{ y: -40, delay: 150 * i }}>{c}</h1>
+        {/each}
+    </div>
+
+    <div class="row center g16">
+        {#each links as [href, text], i}
+            <a {href} class="button" in:fly={{ y: -40, delay: 150 * (i + 1) }}>
+                {text}
+            </a>
+        {/each}
+    </div>
 </div>
 
 <style lang="scss">
+    .letter-row {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        line-height: 0.6;
+    }
+
     div {
         text-align: center;
         padding: 24px 0px;
