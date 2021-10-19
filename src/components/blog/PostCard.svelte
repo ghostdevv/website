be35ec1d995ad964355b028cb504abbcb2b4d93c
GHOSt <ghostdevbusiness@gmail.com>
2021-10-19 21:57:11 +0100

    feat: post cards

---
+++ src/components/blog/PostCard.svelte
@@ -0,0 +1,68 @@
+<article>
+    <img class="image" src="https://via.placeholder.com/1920x1080" alt="Pog" />
+
+    <div class="details">
+        <span class="tag"> BLOG </span>
+
+        <h4 class="title">How to do x in y</h4>
+
+        <p class="description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint iure
+            nesciunt dolore nulla, consectetur placeat rerum esse iusto laborum
+        </p>
+    </div>
+</article>
+
+<style lang="scss">
+    article {
+        $borderRadius: 12px;
+
+        display: flex;
+        flex-direction: column;
+        gap: 0px;
+
+        width: 325px;
+
+        background-color: var(--background-secondary);
+        border-radius: $borderRadius;
+
+        flex-grow: 1;
+
+        .image {
+            width: 100%;
+            height: auto;
+
+            border-radius: $borderRadius $borderRadius 0px 0px;
+        }
+
+        .details {
+            display: flex;
+            flex-direction: column;
+            gap: 4px;
+
+            .tag {
+                margin-bottom: 4px;
+            }
+
+            padding: 18px;
+
+            .tag {
+                background-color: rgba(33, 96, 236, 0.5);
+                color: rgba(33, 96, 236);
+
+                border-radius: 16px;
+
+                padding: 4px 16px;
+                font-size: 11px;
+                font-weight: 800;
+
+                height: fit-content;
+                width: fit-content;
+            }
+
+            .description {
+                color: rgba(var(--text-rgb), 0.6);
+            }
+        }
+    }
+</style>
---
+++ src/components/blog/PostGroup.svelte
@@ -0,0 +1,26 @@
+<script>
+    import PostCard from './PostCard.svelte';
+</script>
+
+<div class="blog-posts">
+    <PostCard />
+    <PostCard />
+    <PostCard />
+    <PostCard />
+    <PostCard />
+    <PostCard />
+    <PostCard />
+</div>
+
+<style lang="scss">
+    .blog-posts {
+        display: flex;
+        flex-wrap: wrap;
+
+        align-items: center;
+        justify-content: center;
+
+        gap: 22px;
+        width: 100%;
+    }
+</style>
