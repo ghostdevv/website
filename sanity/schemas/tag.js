81ccc83dd4365cfe88c79d46c6f2c4874f8e8dd1
GHOSt <ghostdevbusiness@gmail.com>
2021-10-19 22:09:53 +0100

    feat: work on schemas

---
+++ sanity/schemas/tag.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'tag',
+    title: 'Tag',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+        {
+            name: 'colour',
+            title: 'Colour',
+            type: 'color',
+        },
+    ],
+};
