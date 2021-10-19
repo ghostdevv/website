1a2b40074155c5fa0f462b5bdd86e15d7f0522d4
GHOSt <ghostdevbusiness@gmail.com>
2021-10-19 19:46:07 +0100

    refactor: move to component

---
+++ src/components/pages/home/Title.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { featured } from '@/data/links';
+    import { fly } from 'svelte/transition';
+</script>
+
+<section class="column g32 center">
+    <div class="letter-row">
+        {#each 'GHOST' as c, i}
+            <h1 in:fly={{ y: -40, delay: 150 * i }}>{c}</h1>
+        {/each}
+    </div>
+
+    <div class="row center g16">
+        {#each featured as [text, href], i}
+            <a {href} role="button" in:fly={{ y: -40, delay: 150 * (i + 1) }}>
+                {text}
+            </a>
+        {/each}
+    </div>
+</section>
+
+<style lang="scss">
+    .letter-row {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        line-height: 0.6;
+    }
+
+    section {
+        text-align: center;
+        padding: 22px 0px;
+        width: 100%;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,36 +1,5 @@
 <script>
-    import { fly } from 'svelte/transition';
-    import { featured } from '@/data/links';
+    import Title from '@/components/pages/home/Title.svelte';
 </script>
 
-<div>
-    <div class="letter-row">
-        {#each 'GHOST' as c, i}
-            <h1 in:fly={{ y: -40, delay: 150 * i }}>{c}</h1>
-        {/each}
-    </div>
-
-    <div class="row center g16">
-        {#each featured as [text, href], i}
-            <a {href} role="button" in:fly={{ y: -40, delay: 150 * (i + 1) }}>
-                {text}
-            </a>
-        {/each}
-    </div>
-</div>
-
-<style lang="scss">
-    .letter-row {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-
-        line-height: 0.6;
-    }
-
-    div {
-        text-align: center;
-        padding: 24px 0px;
-        width: 100%;
-    }
-</style>
+<Title />
