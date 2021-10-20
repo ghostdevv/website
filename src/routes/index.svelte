666e53f9cc46e1d2251d82d80302fceff94e18e3
GHOSt <ghostdevbusiness@gmail.com>
2021-10-20 18:27:47 +0100

    feat: handle state

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,23 @@
 <script>
     import PostGroup from '@/components/posts/PostGroup.svelte';
     import Title from '@/components/pages/home/Title.svelte';
+    import Loader from '@/components/Loader.svelte';
 
     import { getPosts } from '$sanity';
 
     const postsPromise = getPosts();
+
+    postsPromise.catch(console.error);
 </script>
 
 <Title />
 
 {#await postsPromise}
-    Loading
+    <Loader />
 {:then posts}
     <PostGroup {posts} />
-{:catch e}
-    {e}
+{:catch}
+    <p style="margin: 0 auto;">
+        There was an error loading posts! Contact <mark>GHOST#7524</mark> on discord
+    </p>
 {/await}
