39a769d68a1605b465d18c2dd3c3ed5ae7f50210
GHOSt <ghostdevbusiness@gmail.com>
2021-10-21 13:37:55 +0100

    feat: posts page

---
+++ src/routes/posts.svelte
@@ -0,0 +1,20 @@
+<script>
+    import PostGroup from '@/components/posts/PostGroup.svelte';
+    import Loader from '@/components/Loader.svelte';
+
+    import { getPosts } from '$sanity';
+
+    const postsPromise = getPosts();
+
+    postsPromise.catch(console.error);
+</script>
+
+{#await postsPromise}
+    <Loader />
+{:then posts}
+    <PostGroup {posts} />
+{:catch}
+    <p style="margin: 0 auto;">
+        There was an error loading posts! Contact <mark>GHOST#7524</mark> on discord
+    </p>
+{/await}
