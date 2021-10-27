b7a614eda5bba465db3bda5b3cdee7cfd10bc551
GHOSt <ghostdevbusiness@gmail.com>
2021-10-27 12:25:52 +0100

    feat: fancy animation by fractal

--- src/components/pages/home/Title.svelte
+++ src/components/pages/home/Title.svelte
@@ -1,13 +1,28 @@
 <script>
+    import { quintOut } from 'svelte/easing';
     import { featured } from '@/data/links';
     import { fly } from 'svelte/transition';
+    import { onMount } from 'svelte';
+
+    let mounted = false;
+    onMount(() => (mounted = true));
 </script>
 
 <section class="column g32 center">
     <div class="letter-row">
-        {#each 'GHOST' as c, i}
-            <h1 in:fly={{ y: -20, delay: 150 * i }}>{c}</h1>
-        {/each}
+        {#if mounted}
+            {#each 'GHOST' as c, i}
+                <h1
+                    in:fly={{
+                        x: (i - 2) * 18,
+                        duration: 1500,
+                        easing: quintOut,
+                        delay: 250,
+                    }}>
+                    {c}
+                </h1>
+            {/each}
+        {/if}
     </div>
 
     <div class="row center g16">
