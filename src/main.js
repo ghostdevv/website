058d0f6f6dfabaf063c47b62cef0aff94a5fc5a1
GHOST <ghostdevbusiness@gmail.com>
2021-11-23 23:47:18 +0000

    chore: register service worker

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import 'svelte-hamburgers/dist/css/types/squeeze.css';
 import 'svelte-hamburgers/dist/css/base.css';
 import 'ghostsui/css/ghostsui.css';
+
+import { registerSW } from 'virtual:pwa-register';
 import App from './App.svelte';
+
+// Update/register service worker
+registerSW()();
 
 const app = new App({ target: document.body });
 
