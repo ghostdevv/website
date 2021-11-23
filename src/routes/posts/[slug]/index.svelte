fb27ed3332a7cbcb3216abacf6b1171281be2372
GHOST <ghostdevbusiness@gmail.com>
2021-11-23 23:01:09 +0000

    feat: update card styling

--- src/routes/posts/[slug]/index.svelte
+++ src/routes/posts/[slug]/index.svelte
@@ -14,14 +14,18 @@
 </script>
 
 <article>
-    <img class="image-banner" src={image} alt="Post Banner" />
+    <!-- <img class="image-banner" src={image} alt="Post Banner" /> -->
 
-    <div class="meta">
-        <Tag {...tag} large />
-        <p>{format(new Date(timestamp), 'do LLLL yyyy')}</p>
+    <div class="meta-wrapper" style="--image: url({image})">
+        <div class="meta">
+            <div class="data">
+                <Tag {...tag} large />
+                <p>{format(new Date(timestamp), 'do LLLL yyyy')}</p>
+            </div>
+
+            <h1 class="title">{title}</h1>
+        </div>
     </div>
-
-    <h1 class="title">{title}</h1>
 
     <hr />
 
@@ -43,18 +47,51 @@
             height: 400px;
             max-height: 40vh;
 
-            aspect-ratio: auto;
-
-            padding: 6px 0px;
-            border-radius: 22px;
+            aspect-ratio: 16 / 9;
 
             object-fit: cover;
         }
 
-        .meta {
+        .meta-wrapper {
+            $border-radius: 22px;
+
             display: flex;
-            align-items: center;
-            gap: 12px;
+            flex-direction: column;
+            gap: 16px;
+
+            align-items: flex-start;
+            justify-content: flex-end;
+
+            width: 100%;
+            height: 500px;
+            max-height: 50vh;
+
+            border-radius: $border-radius;
+
+            background-image: var(--image);
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+
+            .meta {
+                display: flex;
+                flex-direction: column;
+                gap: 16px;
+
+                width: 100%;
+                padding: 22px;
+
+                background-color: rgba(var(--background-tertiary-rgb), 0.5);
+                border-radius: 0 0 $border-radius $border-radius;
+
+                backdrop-filter: blur(8px);
+
+                .data {
+                    display: flex;
+                    align-items: center;
+                    gap: 12px;
+                }
+            }
         }
 
         .body {
