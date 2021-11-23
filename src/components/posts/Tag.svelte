a33420aebd52a603eceb3b6f18d0bf7b8de3ef9b
GHOST <ghostdevbusiness@gmail.com>
2021-11-23 14:09:58 +0000

    feat: large class

--- src/components/posts/Tag.svelte
+++ src/components/posts/Tag.svelte
@@ -1,13 +1,15 @@
 <script>
     export let name;
     export let rgb;
+
+    export let large = false;
 </script>
 
-<span class="tag" style="--tag-colour: {rgb.r}, {rgb.g}, {rgb.b}">
+<span class="tag" style="--tag-colour: {rgb.r}, {rgb.g}, {rgb.b}" class:large>
     {name}
 </span>
 
-<style>
+<style lang="scss">
     .tag {
         background-color: rgba(var(--tag-colour), 0.5);
         color: rgba(var(--tag-colour));
@@ -20,5 +22,10 @@
 
         height: fit-content;
         width: fit-content;
+
+        &.large {
+            font-size: 0.85rem;
+            padding: 6px 22px;
+        }
     }
 </style>
