cdd7e21a9c452acf52a60a2e7a4c9c447b3e5981
GHOST <ghostdevbusiness@gmail.com>
2021-11-25 11:41:30 +0000

    feat: offline banner

--- src/routes/_module.svelte
+++ src/routes/_module.svelte
@@ -1,6 +1,14 @@
 <script>
     import Nav from '@/components/Nav.svelte';
+    import { fade } from 'svelte/transition';
+
+    let online;
+    let ignoreOfflineWarning = false;
+
+    $: if (!online) ignoreOfflineWarning = false;
 </script>
+
+<svelte:window bind:online />
 
 <Nav />
 
@@ -9,6 +17,20 @@
         <slot />
     </div>
 </div>
+
+{#if !online && !ignoreOfflineWarning}
+    <banner class="offline red" in:fade>
+        <p>You are offline</p>
+
+        <!-- Ideally should be a button or have role=button but can't since it adds styling - will make a exception class in ghostsui -->
+        <div
+            class="close"
+            style="cursor: pointer"
+            on:click={() => (ignoreOfflineWarning = true)}>
+            <i class="fas fa-times" />
+        </div>
+    </banner>
+{/if}
 
 <style lang="scss">
     div {
@@ -23,6 +45,26 @@
 
         .main {
             padding-top: 140px;
+        }
+    }
+
+    .offline {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+
+        width: 100%;
+
+        display: grid;
+
+        grid-template-columns: 1fr max-content;
+        grid-template-rows: 1fr;
+
+        align-items: center;
+        text-align: center;
+
+        .close {
+            padding: 4px;
         }
     }
 </style>
