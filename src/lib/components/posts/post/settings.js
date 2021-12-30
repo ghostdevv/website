b4a1bd2d4329d0852110fd25a226432ac511fc1a
GHOSt <ghostdevbusiness@gmail.com>
2021-12-30 15:48:29 +0000

    fix: default

--- src/lib/components/posts/post/settings.js
+++ src/lib/components/posts/post/settings.js
@@ -1,4 +1,4 @@
 import { writable } from 'svelte-local-storage-store';
 
-export const fontSize = writable('post-font-size', 18);
+export const fontSize = writable('post-font-size', 16);
 export const codeTheme = writable('post-code-theme', 'material-darker');
