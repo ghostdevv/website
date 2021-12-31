0fe18e3a685d668adf3cae3ac5a1cd827e331ea6
GHOSt <ghostdevbusiness@gmail.com>
2021-12-31 23:38:07 +0000

    fix: sanity

--- src/sanity.js
+++ src/sanity.js
@@ -2,8 +2,7 @@ import client from '@sanity/client';
 
 export default sanity = client({
     projectId: 'x362b5r1',
-    // dataset: import.meta.env.MODE == 'development' ? 'testing' : 'blog',
-    dataset: 'testing',
+    dataset: import.meta.env.MODE == 'development' ? 'testing' : 'blog',
     apiVersion: '2021-03-25',
     useCdn: true,
 });
