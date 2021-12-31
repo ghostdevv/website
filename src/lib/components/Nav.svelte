a8e6ebe6340c1002a1ebf18768870cb9311afe93
GHOSt <ghostdevbusiness@gmail.com>
2021-12-31 15:05:13 +0000

    feat: less dependant on js

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -21,65 +21,65 @@
 
 <svelte:window bind:innerWidth={width} bind:scrollY />
 
-<div class="wrapper" class:mobile class:open class:scroll>
-    {#if $mounted}
-        <nav class:mobile in:fade>
-            <div class="nav-fh">
-                <a href="/">
-                    <div
-                        class="logo"
-                        in:fly={{ y: -20, duration: 750, delay: 100 }}>
-                        <TRainbow />
-                    </div>
-                </a>
-
-                {#if mobile}
-                    <div>
-                        <Hamburger
-                            --color="var(--text)"
-                            type="squeeze"
-                            bind:open />
-                    </div>
-                {/if}
-            </div>
-
-            {#if !$navigating && (open || !mobile)}
-                <div class="nav-sh" transition:slide>
-                    <div class="links">
-                        <a
-                            href="/"
-                            class:active={$page.url.pathname == '/'}
-                            in:fly={{ y: -20, duration: 750 }}>
-                            Home
-                        </a>
-
-                        {#each ['posts', 'projects', 'donate', 'contact'] as link, i (i)}
-                            <a
-                                href="/{link}"
-                                class:active={isActive(`/${link}`)}
-                                in:fly={{
-                                    y: -20,
-                                    duration: 750,
-                                    delay: (i + 1) * 100,
-                                }}>
-                                {link}
-                            </a>
-                        {/each}
-                    </div>
-
-                    <a
-                        href="/donate"
-                        role="button"
-                        class="donate-button"
-                        in:fly={{ y: -20, duration: 750, delay: 100 }}
-                        >Donate</a>
+<div class="wrapper" class:open class:scroll>
+    <nav in:fade>
+        <div class="nav-fh">
+            <a href="/">
+                <div
+                    class="logo"
+                    in:fly={{ y: -20, duration: 750, delay: 100 }}>
+                    <TRainbow />
                 </div>
-            {/if}
-        </nav>
-    {/if}
+            </a>
+
+            <div class="hamburger">
+                <Hamburger --color="var(--text)" type="squeeze" bind:open />
+            </div>
+        </div>
+
+        {#if $mounted && !$navigating && (open || !mobile)}
+            <div
+                class="nav-sh"
+                transition:slide
+                class:open={!$navigating && (open || !mobile)}>
+                <div class="links">
+                    <a
+                        href="/"
+                        class:active={$page.url.pathname == '/'}
+                        in:fly={{ y: -20, duration: 750 }}>
+                        Home
+                    </a>
+
+                    {#each ['posts', 'projects', 'donate', 'contact'] as link, i (i)}
+                        <a
+                            href="/{link}"
+                            class:active={isActive(`/${link}`)}
+                            in:fly={{
+                                y: -20,
+                                duration: 750,
+                                delay: (i + 1) * 100,
+                            }}>
+                            {link}
+                        </a>
+                    {/each}
+                </div>
+
+                <a
+                    href="/donate"
+                    role="button"
+                    class="donate-button"
+                    in:fly={{ y: -20, duration: 750, delay: 100 }}>Donate</a>
+            </div>
+        {/if}
+    </nav>
 </div>
 
 <style lang="scss">
+    @import 'src/lib/helpers/media';
+
+    $mobile: '<900px';
+    $desktop: '>900px';
+
     .wrapper {
         position: fixed;
         z-index: 300;
@@ -94,12 +94,26 @@
 
         transition: border-color 0.2s ease-in-out;
 
-        &.scroll,
-        &.open.mobile {
+        &.scroll {
             box-shadow: 0 4px 10px -2px rgba(0, 0, 0, 0.2),
                 0 4px 20px 0 rgba(0, 0, 0, 0.19);
 
             border-color: var(--primary);
+        }
+
+        @include media($mobile) {
+            &.open {
+                box-shadow: 0 4px 10px -2px rgba(0, 0, 0, 0.2),
+                    0 4px 20px 0 rgba(0, 0, 0, 0.19);
+
+                border-color: var(--primary);
+            }
+        }
+    }
+
+    @include media($desktop) {
+        .hamburger {
+            display: none;
         }
     }
 
@@ -118,7 +132,7 @@
             display: contents;
         }
 
-        &.mobile {
+        @include media($mobile) {
             flex-direction: column;
             justify-content: start;
 
