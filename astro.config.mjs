b6f057a8094729c07ac77e4e647e46a0a28850fb
GHOSt <ghostdevbusiness@gmail.com>
2022-08-13 21:53:41 +0100

    fix: config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,10 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 export default defineConfig({
     integrations: [svelte({})],
+
+    vite: {
+        ssr: {
+            noExternal: ['@fortawesome/*'],
+        },
+    },
 });
