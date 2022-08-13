06a784f2b8a828602dad6c90eba2fdcfd92c0009
GHOSt <ghostdevbusiness@gmail.com>
2022-08-13 21:52:59 +0100

    feat: projects

---
+++ src/components/Projects.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { projects } from '$data/projects';
+</script>
+
+<section class="row wrap center g16">
+    {#each projects as project}
+        <a
+            href={project.url}
+            target="_blank"
+            class="card center"
+            class:star={project.star}>
+            <h5>{project.name}</h5>
+        </a>
+    {/each}
+</section>
+
+<style lang="scss">
+    .star {
+        border-color: var(--primary) !important;
+    }
+</style>
---
+++ src/data/projects.ts
@@ -0,0 +1,120 @@
+export interface Project {
+    name: string;
+    url: string;
+    star?: true;
+}
+
+export const projects: Project[] = [
+    {
+        name: 'Routify',
+        url: 'https://routify.dev/',
+    },
+    {
+        name: 'Svelte Hamburgers',
+        url: 'https://github.com/ghostdevv/svelte-hamburgers',
+        star: true,
+    },
+    {
+        name: 'SourceBin',
+        url: 'https://github.com/ghoststools/sourcebin',
+    },
+    {
+        name: 'JellyCommands',
+        url: 'https://github.com/ghostdevv/jellycommands',
+        star: true,
+    },
+    {
+        name: 'DashArgs',
+        url: 'https://github.com/ghoststools/dashargs',
+    },
+    {
+        name: 'MyKV',
+        url: 'https://github.com/ghostdevv/mykv',
+    },
+    {
+        name: 'GHOSTs Tools',
+        url: 'https://github.com/ghostdevv/ghoststools',
+    },
+    {
+        name: 'djs-ticketsystem',
+        url: 'https://github.com/ghoststools/djs-ticketsystem',
+    },
+    {
+        name: 'Github 1s Button',
+        url: 'https://github.com/ghostdevv/github1s-button',
+    },
+    {
+        name: 'rollup-obfuscator',
+        url: 'https://github.com/ghoststools/rollup-obfuscator',
+    },
+    {
+        name: 'linode-object-upload',
+        url: 'https://github.com/ghostdevv/linode-object-upload',
+    },
+    {
+        name: 'linode-bucket-delete',
+        url: 'https://github.com/ghostdevv/linode-bucket-delete',
+    },
+    {
+        name: 'remark-slug-anchor',
+        url: 'https://www.npmjs.com/package/remark-slug-anchor',
+    },
+    {
+        name: 'Svelte Body',
+        url: 'https://www.npmjs.com/package/svelte-body',
+    },
+    {
+        name: 'greset',
+        url: 'https://www.npmjs.com/package/greset',
+        star: true,
+    },
+    {
+        name: 'sass-hex-rgb',
+        url: 'https://github.com/ghostdevv/sass-hex-rgb',
+    },
+    {
+        name: 'Svelte Mount',
+        url: 'https://github.com/ghostdevv/svelte-mount',
+    },
+    {
+        name: 'Svelte Copy',
+        url: 'https://github.com/ghostdevv/svelte-copy',
+    },
+    {
+        name: 'Create Routify',
+        url: 'https://github.com/roxiness/create-routify',
+    },
+    {
+        name: 'Neru',
+        url: 'https://github.com/NeruJS/neru',
+        star: true,
+    },
+    {
+        name: 'Methods',
+        url: 'https://www.npmjs.com/package/@nerujs/methods',
+    },
+    {
+        name: 'Tasks',
+        url: 'https://github.com/ghostdevv/tasks',
+    },
+    {
+        name: 'BackBlaze CLI',
+        url: 'https://github.com/ghostdevv/backblaze-cli',
+    },
+    {
+        name: 'Svelte Reduced Motion',
+        url: 'https://github.com/ghostdevv/svelte-reduced-motion',
+    },
+    {
+        name: 'GWordle',
+        url: 'https://github.com/ghostdevv/gwordle',
+    },
+    {
+        name: 'TST',
+        url: 'https://github.com/ghostdevv/tst',
+    },
+    {
+        name: 'GHOSTSUi',
+        url: 'https://github.com/ghostdevv/ghostsui',
+    },
+];
