9aa68e811dd935830c5843b8232e62ceea64dbf9
GHOSt <ghostdevbusiness@gmail.com>
2022-08-15 12:10:47 +0100

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
 import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [svelte({}), image()],
+    integrations: [svelte({}), image(), sitemap()],
+
+    site: 'https://ghostdev.xyz',
 
     vite: {
         ssr: {
