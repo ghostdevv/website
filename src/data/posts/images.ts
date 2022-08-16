cd578645c83429a57e17faedd85174cf54e69033
GHOST <ghostdevbusiness@gmail.com>
2022-08-17 00:56:47 +0100

    refactor: fully switch to @astro/image on posts

---
+++ src/data/posts/images.ts
@@ -0,0 +1,21 @@
+import type { ImageMetadata } from '@astrojs/image';
+import { getImage } from '@astrojs/image';
+
+const images: Record<string, { default: ImageMetadata }> = import.meta.glob(
+    './images/**/*',
+    { eager: true },
+);
+
+export function getImageData(image: string): ImageMetadata | undefined {
+    return images[`./images/${image}`]?.default;
+}
+
+export async function getImageSrc(name: string): Promise<string> {
+    const image = await getImage({
+        src: getImageData(name)!,
+        aspectRatio: 16 / 9,
+        width: 1000,
+    });
+
+    return image.src!;
+}
