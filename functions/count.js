551f212e44808289d835d249e8ec1f90bb6e1e27
GHOSt <ghostdevbusiness@gmail.com>
2022-08-21 14:53:52 +0100

    fix: proxy search params

--- functions/count.js
+++ functions/count.js
@@ -1,3 +1,4 @@
-export async function onRequest() {
-    return fetch('https://ghost.goatcounter.com/count');
+export async function onRequest({ request }) {
+    const url = new URL(request.url);
+    return fetch(`https://ghost.goatcounter.com/count${url.search}`);
 }
