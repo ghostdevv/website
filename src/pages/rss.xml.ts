8eb895699c78d7e2bb3e5893bd85e8607f98ff5f
GHOST <ghostdevbusiness@gmail.com>
2023-01-24 20:06:08 +0000

    feat: update rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,22 +1,27 @@
+import { getPosts } from '$helpers/posts';
 import { PostType } from '$data/posts';
-import { mapPosts } from '$data/posts/utils';
 import rss from '@astrojs/rss';
 
-const files = import.meta.glob('./posts/**/*.md', { eager: true });
-const posts = mapPosts(Object.values(files) as Record<string, any>[]);
+export const get = async () => {
+    const posts = await getPosts();
 
-export const get = () =>
-    rss({
+    return rss({
         title: 'GHOSTDev blog',
         description: 'Welcome to the blog of GHOST!',
         site: 'https://ghostdev.xyz',
-        items: posts.map((post) => ({
-            title: post.title,
-            description: post.excerpt,
-            pubDate: new Date(post.timestamp),
-            link:
-                post.type == PostType['Text']
+        items: posts.map((post) => {
+            const link =
+                post.type == PostType.Text
                     ? `https://ghostdev.xyz/posts/${post.slug}`
-                    : post.link!,
-        })),
+                    : post.link;
+
+            return {
+                title: post.title,
+                description: post.excerpt,
+                pubDate: new Date(post.timestamp),
+                draft: false,
+                link,
+            };
+        }),
     });
+};
