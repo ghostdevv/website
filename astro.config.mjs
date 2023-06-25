08adc78bd0eef0c7202bbf13dc9741fce30b82c2
GHOST <ghostdevbusiness@gmail.com>
2023-06-25 19:48:54 +0000

    chore: remove @astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
-// import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [svelte(), /* image(), */ sitemap()],
+    integrations: [svelte(), sitemap()],
 
     site: 'https://ghostdev.xyz/',
 
