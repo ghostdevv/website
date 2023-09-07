80b9ba85cc1acf9f04c8a36a54e99ed91f031c56
GHOST <ghostdevbusiness@gmail.com>
2023-09-07 20:01:38 +0000

    feat: add logo downloads to branding page

---
+++ src/lib/components/branding/Logo.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import { faDownload } from '@fortawesome/free-solid-svg-icons';
+    import Fa from 'svelte-fa';
+
+    interface Format {
+        name: string;
+        url: string;
+    }
+
+    export let name: string;
+    export let formats: Format[];
+</script>
+
+<div class="container">
+    <div class="logo">
+        <slot />
+    </div>
+
+    <h4>{name}</h4>
+
+    {#each formats as { name, url }}
+        <a href={url} target="_blank" download>
+            <Fa icon={faDownload} />
+            {name}
+        </a>
+    {/each}
+</div>
+
+<style lang="scss">
+    .logo {
+        width: 140px;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+    }
+</style>
