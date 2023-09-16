1c627c11cf95c37263fa2d3bbdf6c135acbc9d3e
GHOST <ghostdevbusiness@gmail.com>
2023-09-16 19:39:12 +0000

    feat: podcast tag & content

--- src/data/tags.ts
+++ src/data/tags.ts
@@ -1,9 +1,10 @@
-export const tags = ['blog', 'youtube', 'stream'] as const;
+export const tags = ['blog', 'youtube', 'stream', 'podcast'] as const;
 
 export const tagColours: Record<Tag, string> = {
     blog: '61, 121, 252',
     youtube: '242, 101, 93',
     stream: '201, 86, 247',
+    podcast: '9, 132, 124',
 };
 
 export type Tag = (typeof tags)[number];
