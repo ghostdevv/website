1d3e6c8491a3aa65f1f9b9ea86ecb0258dd7e5a3
GHOST <ghostdevbusiness@gmail.com>
2023-09-16 19:01:49 +0000

    chore: remove twitter tag

--- src/data/tags.ts
+++ src/data/tags.ts
@@ -1,10 +1,9 @@
-export const tags = ['blog', 'youtube', 'stream', 'twitter'] as const;
+export const tags = ['blog', 'youtube', 'stream'] as const;
 
 export const tagColours: Record<Tag, string> = {
     blog: '61, 121, 252',
     youtube: '242, 101, 93',
     stream: '201, 86, 247',
-    twitter: '29, 155, 240',
 };
 
 export type Tag = (typeof tags)[number];
