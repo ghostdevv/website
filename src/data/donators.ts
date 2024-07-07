2a2122bf5a8314fa0ca9e07579f0f920be0c755f
GHOST <ghostdevbusiness@gmail.com>
2024-07-07 02:53:07 +0100

    refactor: use astro:assets

--- src/data/donators.ts
+++ src/data/donators.ts
@@ -7,34 +7,34 @@ export const supporters: { current: Person[]; past: Person[] } = {
     current: [
         {
             name: 'Braden Wiggins',
-            image: '/donators/bruhdy-fractal.webp',
+            image: 'bruhdy-fractal.webp',
         },
     ],
 
     past: [
         {
             name: 'Dusan Malusev',
-            image: '/donators/dusan.webp',
+            image: 'dusan.webp',
         },
         {
             name: 'Stefan Bogdanović',
-            image: '/donators/stefan.webp',
+            image: 'stefan.webp',
         },
         {
             name: 'Scott Spence',
-            image: '/donators/scott-spence.webp',
+            image: 'scott-spence.webp',
         },
         {
             name: 'Alfie',
-            image: '/donators/alfie.webp',
+            image: 'alfie.webp',
         },
         {
             name: 'Cain',
-            image: '/donators/cain.webp',
+            image: 'cain.webp',
         },
         {
             name: 'Gen Ashley',
-            image: '/donators/gen-ashley.webp',
+            image: 'gen-ashley.webp',
         },
     ],
 };
@@ -42,10 +42,10 @@ export const supporters: { current: Person[]; past: Person[] } = {
 export const donators: Person[] = [
     {
         name: 'Kevin Åberg Kultalahti',
-        image: '/donators/kevin.webp',
+        image: 'kevin.webp',
     },
     {
         name: 'ChristopherMc',
-        image: '/donators/christophermc.webp',
+        image: 'christophermc.webp',
     },
 ];
