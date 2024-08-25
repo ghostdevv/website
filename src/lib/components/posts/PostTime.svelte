fea25fbedf5ae6fce336f02ca8723392ce067e92
GHOST <ghostdevbusiness@gmail.com>
2024-08-25 03:51:39 +0100

    feat: update post header

---
+++ src/lib/components/posts/PostTime.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { format, formatDistanceToNow } from 'date-fns';
+
+	interface Props {
+		lastEdited?: number;
+		postedAt: number;
+	}
+
+	let { lastEdited, postedAt }: Props = $props();
+</script>
+
+{#snippet date(timestamp: number)}
+	<span title={format(timestamp, 'do LLLL yyyy HH:mm')}>
+		{formatDistanceToNow(timestamp, { addSuffix: true })}
+	</span>
+{/snippet}
+
+
+<p>
+	Posted {@render date(postedAt)}{lastEdited ? ',' : ''}
+
+	{#if lastEdited}
+		updated {@render date(lastEdited)}
+	{/if}
+</p>
+
+<style lang="scss">
+	p {
+		color: rgba(var(--text-rgb), 0.8);
+		line-height: 1.6;
+		font-size: 14px;
+		margin: 6px 0px;
+	}
+</style>
