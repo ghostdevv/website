5c687f6070420826b5bf82731c2dde4c8b0f09b9
Willow (GHOST) <git@willow.sh>
2026-03-11 06:43:18 +0000

    fix: build

--- scripts/embed.ts
+++ scripts/embed.ts
@@ -1,7 +1,7 @@
 import { readFile, appendFile, rm } from 'node:fs/promises';
 import { join } from 'node:path';
 
-const DIST = join(import.meta.dirname, '../dist');
+const DIST = join(import.meta.dirname, '../dist/client');
 
 const embed = JSON.parse(await readFile(`${DIST}/embed.json`, 'utf-8')) as {
 	image: string;
