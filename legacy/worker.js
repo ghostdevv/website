96c989f799dc06c37a6ce52128b63628faf58d2c
Willow (GHOST) <git@willow.sh>
2026-05-09 00:00:59 +0100

    fix: hopefully working redirect worker

---
+++ legacy/worker.js
@@ -0,0 +1,27 @@
+/** @param {string} path */
+const notice = (path) => `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>moving ->></title>
+</head>
+<body>
+    <p>I've moved to <a href="https://willow.sh${path}">willow.sh</a>!</p>
+</body>
+</html>`;
+
+export default {
+	/** @param {Request} request */
+	async fetch(request) {
+		const url = new URL(request.url);
+
+		if (url.pathname === '/rss.xml') {
+			return await fetch('https://willow.sh/rss.xml');
+		}
+
+		return new Response(notice(state.url.pathname), {
+			headers: { 'Content-Type': 'text/html' },
+		});
+	},
+};
